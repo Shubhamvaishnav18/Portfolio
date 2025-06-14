26eac341ae0e4cd80b91b7b84862367f60866d03
Shubham Vaishnav <159063010+Shubhamvaishnav18@users.noreply.github.com>
2025-06-14 23:26:47 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,12 +4,14 @@ import Skills from '../../Components/Skills/Skills';
 import Projects from '../../Components/Projects/Projects';
 import About from '../../Components/About/About';
 import Contact from '../../Components/Contact/Contact';
+import Experience from '../../Components/Experience/Experience';
 
 const Home = () => {
     return (
         <>
         <Hero />
         <Skills />
+        <Experience />
         <Projects />
         <Contact />
         </>
