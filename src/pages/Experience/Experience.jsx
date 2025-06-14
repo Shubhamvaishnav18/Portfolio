3b4f54fd0e5d9414650efd93eccae8e017e75f03
Shubham Vaishnav <159063010+Shubhamvaishnav18@users.noreply.github.com>
2025-06-14 23:25:59 +0530

    Add files via upload

---
+++ src/pages/Experience/Experience.jsx
@@ -0,0 +1,12 @@
+import {React} from 'react';
+import Experience from '../../Components/Experience/Experience';
+
+const Projectspage = () => {
+    return (
+        <>
+        <Experience />
+        </>
+  );
+};
+
+export default Projectspage;
